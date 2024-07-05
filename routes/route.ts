a7fda441a35fbdaeb29bf675332db1315df9ad15
muhfachriannoor <muhammadfachriannoor@gmail.com>
2024-07-06 06:38:42 +0800

    refactor: test and route

---
+++ routes/route.ts
@@ -0,0 +1,38 @@
+import "dotenv/config";
+import "../config/filealias";
+import "@Config/database";
+import express, { Express, Request, Response } from "express";
+import { authorize, checkRole } from "@Middlewares/authorization";
+import { routeNotFound } from "@Middlewares/routeNotFound";
+import swaggerUI from "swagger-ui-express";
+import carsRouter from "@Routes/Cars/CarsRoute";
+import usersRouter from "@Routes/Users/UsersRoute";
+import authRouter from "@Routes/Auth/AuthRoute";
+import cors from "cors";
+import YAML from "yamljs";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response): void => {
+  res.status(200).json({
+    message:
+      "Hello, this Restful API Challenge SYNRGY 7 build from Muhammad Fachrian Noor",
+  });
+});
+
+app.use(
+  "/api/docs",
+  swaggerUI.serve,
+  swaggerUI.setup(YAML.load("api-ch6-synrgy-muhfachriannoor-docs.yaml"))
+); // Dokumentasi API
+
+app.use("/api/cars", carsRouter); //Cars Router
+app.use("/api/users", [authorize, checkRole(["SUPERADMIN"])], usersRouter); //Users Router
+app.use("/api", authRouter); //Auth Router
+app.use(routeNotFound); // Custom Route Not Found
+
+export default app;
--- tests/cars.test.ts
+++ tests/cars.test.ts
@@ -1,10 +1,51 @@
 import request from "supertest";
-import app from "../routes/server";
+import app from "../routes/route";
 
 describe("testing /api/cars/list-available", () => {
-  it("it should return 200 without filtering", async () => {
-    await request(app).get("/api/cars/list-available")
-    .expect("Content-Type", "application/json; charset=utf-8")
-    .expect(200);
+   it("it should return 200 without filtering", async () => {
+     await request(app)
+       .get("/api/cars/list-available")
+       .expect("Content-Type", "application/json; charset=utf-8")
+       .expect(200)
+       .then((response) => {
+         expect(response.body.message).toBe("Success Get All Cars Available");
+         expect(response.body.total).toBeGreaterThanOrEqual(0);
+         expect(response.body.data.cars).toBeInstanceOf(Array);
+       });
+   });
+
+  it("it should return 200 with filtering", async () => {
+    const typeDriver: string = "With Driver";
+    const date: string = "2024-01-01";
+    const pickTime: string = "06:00";
+    const totalPassenger: string = "4";
+
+    await request(app)
+      .get(
+        `/api/cars/list-available?typeDriver=${typeDriver}&date=${date}&pickTime=${pickTime}&totalPassenger=${totalPassenger}`
+      )
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.message).toBe("Success Get Filter All Cars Available");
+        expect(response.body.total).toBeGreaterThanOrEqual(0);
+        expect(response.body.data.cars).toBeInstanceOf(Array);
+      });
+  });
+
+  it("it should return 404 filtering not found", async () => {
+    const typeDriver2: string = "With Driver";
+    const date2: string = "2025-01-01";
+    const pickTime2: string = "06:00";
+    const totalPassenger2: string = "4";
+
+    await request(app)
+      .get(`/api/cars/list-available?typeDriver=${typeDriver2}&date=${date2}&pickTime=${pickTime2}&totalPassenger=${totalPassenger2}`)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.message).toBe("Data not found");
+        expect(response.body.data).toBeInstanceOf(Object);
+      });
   });
 });
